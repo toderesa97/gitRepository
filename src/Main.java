72fbac27587f06ed1ca3c225093f4cf20d8473c4
test <toderesa97@gmail.com>
2017-05-30 21:53:07 +0200

    first commit using git :)

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by test on 28/05/2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
---
+++ src/Query.java
@@ -0,0 +1,12 @@
+/**
+ * Created by test on 30/05/2017.
+ */
+public class Query {
+    private String queryName;
+
+    public Query (String queryName) {
+        this.queryName = queryName;
+    }
+
+    
+}
