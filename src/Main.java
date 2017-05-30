2ea9db33e0a86edd27a8a0304c30454140c0b656
test <toderesa97@gmail.com>
2017-05-30 22:07:33 +0200

    add two variables and a print with their sum

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, this is a print message");
+        int a = 5;
+        int b = 7;
+
+        String toBePrint = String.format("%d+%d=%d", a, b, a+b);
+        System.out.println(toBePrint);
     }
 
 }
